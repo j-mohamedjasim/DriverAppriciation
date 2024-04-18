640553846ec64434f8ecdd67f291ce80378aa382
Mohamed Jasim <167366935+j-mohamedjasim@users.noreply.github.com>
2024-04-18 19:50:37 +0100

    Add files via upload
    This python file is used to ease writing same thing over and over again to send driver appreciation message when they have good feedback from the customer with little auto changes. What I could have done is auto-email and whats app message is being sent to the drivers when they receive. The reason why I didn't is because of my work computer would not allow these to run.

---
+++ dApp.py
@@ -0,0 +1,258 @@
+driver = ['Kwebena Aygemang', 'Donovan King', 'Gary Saxon','Yrim Fejullahu','Danilo Da Santo','Hathem Jasem','Paul Robert','Jose De Chunya','Paranamanage Gunasekara','Sandro Analdo','Nazim Uddin','Manjeet Singh','Laurynus Goukas','Chezary Imbierowicz','William Day','Aston Bugeja','Michael Jackson','Tony Dunen','Margaret Thomas','Miguel Joao','Rrustem Statovci','Baraka Sesay','Sammy Dadson','Bessim Fejzullahu','Joseph Muniz','John Christy','Mario Du Traipires','Danny Mills','Paul Maloney','Joao Ferrerira','Jefferson Olveira','Rob Rose','Delsio Seixas','Bashkim Fejullahu','St Clair Barlett','Alek Aleksiev','Joao Santos', 'Dragos Getea','Pedro Alves','Rafael Moraes','Jonathan Green','Jose Franco','Beto Gasper','Anthony Brace','Hitalo Silva','Sorin Micu','Glen Noller','Marcelo Andrade','Lukaz Lukosius','Jose Junior','Juliano Pavarine']
+
+round = {'101': {'name': 'Sorin Micu', 'route': 101, 'contact': '07470 580553', 'company': 'Jake Webster','postcode': 'E16 2, E16 3'},
+         '102': {'name': 'Sammy Dadson', 'route': 102, 'contact': '07957 115703', 'company': 'Jake Webster','postcode': 'E16 1, E16 4'},
+         '103': {'name': 'Chezary Imberowicz', 'route': 103, 'contact': '07721 309327', 'company': 'Jake Webster','postcode': 'E14 0, E14 2, E14 3, E14 8'},
+         '104': {'name': 'Paranamange Jayntha', 'route': 104, 'contact': '07403 102725', 'company': 'Jake Webster','postcode': 'E1 0, E14 6, E14 7'},
+         '105': {'name': 'Laurynus Guokas', 'route': 105, 'contact': '07453 035420', 'company': 'Jake Webster','postcode': 'E1 1, E1 4'},
+         '106': {'name': 'William Day', 'route': 106, 'contact': '07701 362528', 'company': 'Jake Webster','postcode': 'E1 8, E1W 1, E1W 2, E1W 3'},
+         '107': {'name': 'Joao Ferrerira', 'route': 107, 'contact': '07459 154968', 'company': 'Jake Webster','postcode': 'E1 6, E1 7'},
+         '108': {'name': 'Pedro Alves', 'route': 108, 'contact': '07930 999015', 'company': 'Jake Webster','postcode': 'E1 2, E1 3, E1 5'},
+         '109': {'name': 'Chamara Jayawardena', 'route': 109, 'contact': '07947 906287', 'company': 'Jake Webster','postcode': 'E14 4, E14 5'},
+         '110': {'name': 'Anthony Brace', 'route': 110, 'contact': '07988 509455', 'company': 'Jake Webster','postcode': 'E14 9'},
+         '111': {'name': 'Charlie Pullem', 'route': 111, 'contact': '07730 336743', 'company': 'Jake Webster','postcode': 'Sweep'},
+         '121': {'name': 'Marcelo Andrade', 'route': 121, 'contact': '07477 524180', 'company': 'Henrique Mariani','postcode': 'E20 1, E20 2, E20 3, E3 3'},
+         '122': {'name': 'Margaret Thomas', 'route': 122, 'contact': '07958 912117', 'company': 'Henrique Mariani','postcode': 'E8 3, E8 4'},
+         '123': {'name': 'Ricardo Menezes', 'route': 123, 'contact': '07950 350666', 'company': 'Henrique Mariani','postcode': 'E8 1, E8 2'},
+         '124': {'name': 'Joseph Muniz', 'route': 124, 'contact': '07761 721276', 'company': 'Henrique Mariani','postcode': 'E3 2, E3 5'},
+         '125': {'name': 'Raymond Chisakuwana', 'route': 125, 'contact': '07591 864328', 'company': 'Henrique Mariani','postcode': 'E2 7, E2 8, E2 6'},
+         '126': {'name': 'Rafael Moraes', 'route': 126, 'contact': '07547 506082', 'company': 'Henrique Mariani','postcode': 'E2 0, E2 2, E2 9, E3 4'},
+         '127': {'name': 'Hathem Jasem', 'route': 127, 'contact': '07878 687581', 'company': 'Henrique Mariani','postcode': 'E9 6, E9 7, E9 5'},
+         '141': {'name': 'Jose Da Chunya', 'route': 141, 'contact': '07440 450991', 'company': 'Baraka Sesay','postcode': 'IG10 1, IG10 2, IG10 3, IG10 4'},
+         '142': {'name': 'Nazim Uddin', 'route': 142, 'contact': '07951 475542', 'company': 'Baraka Sesay','postcode': 'IG3 8, IG3 9, IG2 7, IG1 4'},
+         '143': {'name': 'Manjeet Singh', 'route': 143, 'contact': '07958 048370', 'company': 'Baraka Sesay','postcode': 'IG4 5, IG2 6, IG5 0, IG6 1'},
+         '144': {'name': 'Sandro Analdo', 'route': 144, 'contact': '07421 995389', 'company': 'Baraka Sesay','postcode': 'E18 1, E18 2, IG8 8, IG8 7'},
+         '145': {'name': 'Delcio Seixas', 'route': 145, 'contact': '07599 754659', 'company': 'Baraka Sesay','postcode': 'IG7 4, IG6 2, IG6 3, IG7 6'},
+         '146': {'name': 'Jose Franco', 'route': 146, 'contact': '07459 643037', 'company': 'Baraka Sesay','postcode': 'IG9 5, IG9 6, IG8 0, IG8 9'},
+         '161': {'name': 'Danilo Da Santos', 'route': 161, 'contact': '07300 816432', 'company': 'Bessim Fejzullahu','postcode': 'IG11 0'},
+         '162': {'name': 'Yrim Fejzullahu', 'route': 162, 'contact': '07949 642904', 'company': 'Bessim Fejzullahu','postcode': 'IG11 7, IG11 8, IG11 9'},
+         '163': {'name': 'Miguel Joao', 'route': 163, 'contact': '07466 491855', 'company': 'Bessim Fejzullahu','postcode': 'E13 0, E13 8, E13 9, E6 1'},
+         '164': {'name': 'Mario Phillipe Du Trapires', 'route': 164, 'contact': '07429 654241', 'company': 'Bessim Fejzullahu','postcode': 'E6 2, E6 3, E6 5, E6 6, E6 7'},
+         '165': {'name': 'Gary Saxon', 'route': 165, 'contact': '07956 074608', 'company': 'Bessim Fejzullahu','postcode': 'E11 1, E11 2, E11 3, E11 4'},
+         '166': {'name': 'Joao Santos', 'route': 166, 'contact': '07438 327007', 'company': 'Bessim Fejzullahu','postcode': 'E10 5, E10 6, Overspill'},
+         '167': {'name': 'Carlos Teixeira', 'route': 167, 'contact': '07465 015319', 'company': 'Bessim Fejzullahu','postcode': 'E10 7'},
+         '168': {'name': 'Beto Gasper', 'route': 168, 'contact': '07454 792765', 'company': 'Bessim Fejzullahu','postcode': 'E15 1, E15 2, E15 3'},
+         '169': {'name': 'Rob Rose', 'route': 169, 'contact': '07960 834107', 'company': 'Bessim Fejzullahu','postcode': 'E7 8, E7 9, E15 3'},
+         '170': {'name': 'Donovan King', 'route': 170, 'contact': '07377 518278', 'company': 'Bessim Fejzullahu','postcode': 'E12 5, E12 6, E7 0'},
+         '171': {'name': 'Almirio Pamente', 'route': 171, 'contact': '07480 407429', 'company': 'Bessim Fejzullahu','postcode': 'IG1 1, IG1 2, IG1 3'},
+         '201': {'name': 'Kwebena Aygemang', 'route': 201, 'contact': '07931 817672', 'company': 'Mick Hegarty','postcode': 'RM13 7, RM13 9'},
+         '203': {'name': 'John Christy', 'route': 203, 'contact': '07960 685175', 'company': 'Mick Hegarty','postcode': 'RM4 1, RM5 2, RM5 3'},
+         '204': {'name': 'Aston Bugjea', 'route': 204, 'contact': '07535 412519', 'company': 'Mick Hegarty','postcode': 'RM7 0, RM7 7, RM7 8, RM7 9'},
+         '205': {'name': 'Paul Maloney', 'route': 205, 'contact': '07577 229934', 'company': 'Mick Hegarty','postcode': 'RM10 7, RM10 8, RM10 9'},
+         '206': {'name': 'Ian Miller', 'route': 206, 'contact': '07790 327305', 'company': 'Mick Hegarty','postcode': 'RM9 6, RM13 8'},
+         '207': {'name': 'Alek Alexsiev', 'route': 207, 'contact': '07889 419518', 'company': 'Mick Hegarty','postcode': 'RM8 2, RM8 3, RM9 4, RM9 5'},
+         '301': {'name': 'John Abraham', 'route': 301, 'contact': '07930 245848', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '302': {'name': 'St Clair Bartlett', 'route': 302, 'contact': '07947 664082', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '303': {'name': 'Francis Lawler', 'route': 303, 'contact': '07517 556996', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '304': {'name': 'Luizi Kalombo', 'route': 304, 'contact': '07946 666502', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '305': {'name': 'Anthony Elliot', 'route': 305, 'contact': '07948 716156', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '306': {'name': 'Glen John Noller', 'route': 306, 'contact': '07956 494038', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '307': {'name': 'Paul Roberts', 'route': 307, 'contact': '07925 164100', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '308': {'name': 'Amjid Gul', 'route': 308, 'contact': '07456 032347', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '309': {'name': 'Jonathan Green', 'route': 309, 'contact': '07956 494038', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '310': {'name': 'Gary Tyler', 'route': 310, 'contact': '07753 372376', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '380': {'name': 'Anthony Jennings', 'route': 380, 'contact': '07932 656157', 'company': 'Mick Hegarty','postcode': 'BULK ROUTE'},
+         '442': {'name': 'Jefferson Olveira', 'route': 442, 'contact': '07510 258261', 'company': 'Henrique Mariani','postcode': 'Additional Routes'},
+        
+         }
+
+name = []
+boss = []
+route = []
+feedback = []
+score = []
+
+while True:
+    
+    rno = input('What is the round number? ')
+
+    if rno in round:
+        feed = input('What is the feedback? ')
+        scor = input('What is his/her overall score? ')
+        drive = round[rno]['name']
+        bossname = round[rno]['company']
+        routenumber = round[rno]['route']
+
+        name.append(drive)
+        boss.append(bossname)
+        route.append(routenumber)
+        feedback.append(feed)
+        score.append(scor)
+
+        print(' ')
+        print('Driver Name: ',name[0])
+        print('Boss Name: ',boss[0])
+        print(' ')
+
+        correction = input('Is this correct? ')
+
+        if correction == 's':
+            pass
+        elif correction == 'n':
+             name.pop(0)
+             boss.pop(0)
+             #route.pop(0)
+             #feedback.pop(0)
+             #score.pop(0)
+
+             dname = input('What is the driver name? ')
+             bname = input('What is his/her boss name? ')
+             #rnumber = input('What is the round number? ')
+             #feed = input('What is the feedback? ')
+             #scor = input('What is his/her overall score? ')
+
+             name.append(dname)
+             boss.append(bname)
+             #route.append(rnumber)
+             #feedback.append(feed)
+             #score.append(scor)
+
+             print('Boss Email')
+             print(' ')
+             print('Hi ',boss[0].capitalize(),',')
+             print(' ')
+             print('Good feedback was received on route ',route[0], name[0].capitalize(),'. Please see the feedback below:')
+             print(' ')
+             print('Feedback: ',feedback[0])
+             print(' ')
+             print('Customer satisfaction score: ',score[0],'/10')
+             print(' ')
+             print(' ')
+             print(' ')
+             print(' ')
+
+            #driver message print   
+             print('Drivers message')
+             print('Good Work! ', name[0].capitalize(),'👏 ', ' you received a positive feedback from the customer on this delivery. Please read the customer feedback below.')
+             print(' ')
+             print('Feedback: ',feedback[0])
+             print(' ')
+             print('Customer satisfaction score: ',score[0],'/10')
+             print(' ')
+             print(' ')
+             print(' ')
+             print(' ')
+
+             name.pop(0)
+             boss.pop(0)
+             route.pop(0)
+             feedback.pop(0)
+             score.pop(0)
+        else:
+            print('Your entry is invalid. Please type [y/n]')
+            name.pop(0)
+            boss.pop(0)
+            route.pop(0)
+            feedback.pop(0)
+            score.pop(0)
+        
+
+
+    elif rno == 'end':
+        print('Boss Email')
+        print(' ')
+        print('Hi ',boss[0],',')
+        print(' ')
+        print('Good feedback was received on route ',route[0], name[0],'. Please see the feedback below:')
+        print(' ')
+        print('Feedback: ',feedback[0])
+        print(' ')
+        print('Customer satisfaction score: ',score[0],'/10')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+
+        #driver message print   
+        print('Drivers message')
+        print(' ')
+        print('Good Work! ', name[0],'👏 ', ' you received a positive feedback from the customer on this delivery. Please read the customer feedback below.')
+        print(' ')
+        print('Feedback: ',feedback[0])
+        print(' ')
+        print('Customer satisfaction score: ',score[0],'/10')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+
+        name.pop(0)
+        boss.pop(0)
+        route.pop(0)
+        feedback.pop(0)
+        score.pop(0)
+
+    elif rno == 'k':
+        break
+    
+
+    elif rno not in round:
+        print('The round number is does not exist')
+
+        alert = input('Are you sure this is the right round number? ')
+
+        if alert == 's':
+            dname = input('What is the driver name? ')
+            bname = input('What is his/her boss name? ')
+            rnumber = input('What is the round number? ')
+            feed = input('What is the feedback? ')
+            scor = input('What is his/her overall score? ')
+
+            name.append(dname)
+            boss.append(bname)
+            route.append(rnumber)
+            feedback.append(feed)
+            score.append(scor)
+
+            print('Boss Email')
+            print(' ')
+            print('Hi ',boss[0].capitalize(),',')
+            print(' ')
+            print('Good feedback was received on route ',route[0], name[0].capitalize(),'. Please see the feedback below:')
+            print(' ')
+            print('Feedback: ',feedback[0])
+            print(' ')
+            print('Customer satisfaction score: ',score[0],'/10')
+            print(' ')
+            print(' ')
+            print(' ')
+            print(' ')
+
+            #driver message print   
+            print('Drivers message')
+            print('Good Work! ', name[0].capitalize(),'👏 ', ' you received a positive feedback from the customer on this delivery. Please read the customer feedback below.')
+            print(' ')
+            print('Feedback: ',feedback[0])
+            print(' ')
+            print('Customer satisfaction score: ',score[0],'/10')
+            print(' ')
+            print(' ')
+            print(' ')
+            print(' ')
+
+            name.pop(0)
+            boss.pop(0)
+            route.pop(0)
+            feedback.pop(0)
+            score.pop(0)
+
+        else:
+            print('Please enter the valid round number')
+            pass
+
+    
+
+    elif rno == 'k':
+        break
+        
+    else:
+        print('Your entry is invalid')
+        print(' ')
+        print('Please type [k] to kill OR [end] to print values')
+
+
+    if drive in driver:
+        na = driver.index(drive)
+        print('The driver name is in index',na)
+        print()
+        print()
